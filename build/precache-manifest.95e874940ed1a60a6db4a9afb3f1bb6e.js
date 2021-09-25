ccaa5e7bd2e863d958c5b02725e3b5f7cf778dd6
nasir733 <engnasir733@gmail.com>
2021-09-25 11:38:57 +0500

    v1.0.1

---
+++ build/precache-manifest.95e874940ed1a60a6db4a9afb3f1bb6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "687acde711010b31ab94048a31700090",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6505a0f5fad63358478",
+    "url": "/static/css/2.8caad30d.chunk.css"
+  },
+  {
+    "revision": "65543c20b24b584851c0",
+    "url": "/static/css/main.c5aae64c.chunk.css"
+  },
+  {
+    "revision": "b6505a0f5fad63358478",
+    "url": "/static/js/2.bcd91576.chunk.js"
+  },
+  {
+    "revision": "e74d0796d6a0682403ff1efda5043636",
+    "url": "/static/js/2.bcd91576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65543c20b24b584851c0",
+    "url": "/static/js/main.478015fc.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "e9798857d9dc1f7e07b7eaaf24a6cc90",
+    "url": "/static/media/download.e9798857.jfif"
+  }
+]);
